816926a14b04420a9e077d701030e7bcca93577d
nitin9924 <nitin.iblinfotech@gmail.com>
2020-04-07 17:21:49 +0530

    change the data field

--- Api_Demo/Models/Demo_Model.Designer.cs
+++ Api_Demo/Models/Demo_Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\IBL\Api_Demo\Api_Demo\Models\Demo_Model.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Employee_Crud\EmployeeCrude\Api_Demo\Models\Demo_Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
